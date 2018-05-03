0031fc615764cf2c9e01e63f40447862e3cfc116
Hani Lim <hani.lim@dowjones.com>
2018-05-03 12:04:17 -0400

    notes for render()

--- client/index.js
+++ client/index.js
@@ -1,3 +1,15 @@
+/*
+This is the JS that runs on the browser
+It imports our APP and places it on the page
+
+render() in this context is equivalent to ReactDOM.render()
+
+render(
+  <h1>Hello World</h1>,
+  document.getElementById('root')
+);
+*/
+
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
